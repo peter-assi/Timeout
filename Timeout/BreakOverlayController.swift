2aef20ca877e722767b540e85829297534b6be89
Peter Assi <peter@lind.be>
2026-04-16 11:30:43 +0200

    Tune overlay animation cadence

--- Timeout/BreakOverlayController.swift
+++ Timeout/BreakOverlayController.swift
@@ -466,7 +466,7 @@ private struct ExerciseMotionView: View {
 
     var body: some View {
         GeometryReader { proxy in
-            TimelineView(.animation(minimumInterval: 1.0 / 15.0)) { timeline in
+            TimelineView(.animation(minimumInterval: 1.0 / 20.0)) { timeline in
                 let size = proxy.size
                 let time = timeline.date.timeIntervalSinceReferenceDate
 
